ad53a40aee3d74a1fe3f7a81ac98345981c655ce
Kyle Bensink <kyle.r.bensink.21@dartmouth.edu>
2021-03-06 14:47:14 -0500

    adding filtering by hasReviewText

--- src/schema/dishrating.js
+++ src/schema/dishrating.js
@@ -1,10 +1,26 @@
+/* eslint-disable no-unused-vars */
 import { DishRating, DishRatingTC } from '../models/dishrating.js';
 
 const DishRatingQuery = {
   dishRatingById: DishRatingTC.getResolver('findById'),
   dishRatingByIds: DishRatingTC.getResolver('findByIds'),
   dishRatingOne: DishRatingTC.getResolver('findOne'),
-  dishRatingMany: DishRatingTC.getResolver('findMany'),
+  dishRatingMany: DishRatingTC.getResolver('findMany')
+                              .addFilterArg({
+                                name: 'hasReviewText',
+                                type: 'Boolean', // also can be 'Int Float Boolean ID String! [String] AnyNamedType' 
+                                description: 'Search by regExp',
+                                query: (rawQuery, value, resolveParams) => {
+                                  if (value) {
+                                    // eslint-disable-next-line no-useless-escape
+                                    rawQuery.review = new RegExp("^(?!\s*$).+");
+                                  }
+                                  else {
+                                    // eslint-disable-next-line no-useless-escape
+                                    rawQuery.review = new RegExp("^$");
+                                  }
+                                },
+                              }),
   dishRatingCount: DishRatingTC.getResolver('count'),
   dishRatingConnection: DishRatingTC.getResolver('connection'),
   dishRatingPagination: DishRatingTC.getResolver('pagination'),
